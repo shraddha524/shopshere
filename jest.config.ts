320eb8b92706bb54d4f80b365b36d39e9edf826e
Shraddha <shraddhamoily392@gmail.com>
2025-09-03 18:28:12 +0530

    Add files via upload

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,21 @@
-import type { Config } from 'jest';
-import { getJestProjectsAsync } from '@nx/jest';
+/* eslint-disable */
+import { readFileSync } from 'fs';
 
-export default async (): Promise<Config> => ({
-  projects: await getJestProjectsAsync()
-});
+// Reading the SWC compilation config for the spec files
+const swcJestConfig = JSON.parse(
+  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+);
+
+// Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
+swcJestConfig.swcrc = false;
+
+export default {
+  displayName: '@eshop/eshop',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: 'test-output/jest/coverage'
+};
---
+++ main.ts
@@ -0,0 +1,17 @@
+
+import express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
